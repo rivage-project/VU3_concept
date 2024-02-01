8e00074220e8f38e484f596390b175a3ff802db0
Clara Marino <claramarino665@gmail.com>
2024-02-01 15:12:15 +0100

    init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' VU3_concept: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Clara Marino \email{claramarino665@gmail.com}
+#' 
+#' @date 2024/02/01
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))

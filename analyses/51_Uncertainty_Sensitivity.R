e1dfcc2f1913084e79c72451ad965607c7352799
denelle <pierre.denelle@gmail.com>
2024-11-07 15:06:09 +0100

    Uncertainty_Sensitivity updated :art:

--- analyses/51_Uncertainty_Sensitivity.R
+++ analyses/51_Uncertainty_Sensitivity.R
@@ -59,7 +59,6 @@ normaliseFUN <- function(norm_method, df) {
   return(df)    
 }
 
-
 sensitivityFUN <- function(trait_df,
                            trait_columns,
                            norm_method){
@@ -84,80 +83,6 @@ sensitivityFUN <- function(trait_df,
   outDF <- cbind(trait_df_norm, outDF[, 'sensitivity_minmax', drop = FALSE])
   
   return(outDF)
-  
-  # # Normalize each marker
-  # if(norm == "minmax"){
-  #   # Min-max normalization
-  #   trait_df_minmax <- apply(trait_df[, tracol], 2, function(x){
-  #     (x - min(x, na.rm = TRUE))/(max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
-  #   })
-  #   colnames(trait_df_minmax) <- paste0(colnames(trait_df_minmax), "_minmax")
-  #   
-  #   trait_df_minmax <- as.data.frame(trait_df_minmax)
-  #   
-  #   # Calculating the Sensitivity Index
-  #   trait_df_minmax$sensitivity_minmax <-
-  #     apply(trait_df_minmax, 1, function(x) sum(x, na.rm = TRUE))
-  #   
-  #   # Normalizing the Sensitivity Index
-  #   trait_df_minmax$sensitivity_minmax_norm <-
-  #     (trait_df_minmax$sensitivity_minmax -
-  #        min(trait_df_minmax$sensitivity_minmax, na.rm = TRUE))/
-  #     (max(trait_df_minmax$sensitivity_minmax, na.rm = TRUE) -
-  #        min(trait_df_minmax$sensitivity_minmax, na.rm = TRUE))
-  #   
-  #   # Binding normalization to the original trait dataset
-  #   trait_df <- cbind(trait_df, trait_df_minmax)
-  #   
-  # } else if(norm == "logminmax"){
-  #   # Log minmax normalization
-  #   trait_df_logminmax <- apply(trait_df[, tracol], 2, function(x){
-  #     (log(x + 1) - min(log(x + 1), na.rm = TRUE))/(max(log(x + 1), na.rm = TRUE) - min(log(x + 1), na.rm = TRUE))
-  #   })
-  #   colnames(trait_df_logminmax) <- paste0(colnames(trait_df_logminmax), "_logminmax")
-  #   
-  #   trait_df_logminmax <- as.data.frame(trait_df_logminmax)
-  #   
-  #   # Calculating the Sensitivity Index
-  #   trait_df_logminmax$sensitivity_logminmax <-
-  #     apply(trait_df_logminmax, 1, function(x) sum(x, na.rm = TRUE))
-  #   
-  #   # Normalizing the Sensitivity Index
-  #   trait_df_logminmax$sensitivity_logminmax_norm <-
-  #     (trait_df_logminmax$sensitivity_logminmax -
-  #        min(trait_df_logminmax$sensitivity_logminmax, na.rm = TRUE))/
-  #     (max(trait_df_logminmax$sensitivity_logminmax, na.rm = TRUE) -
-  #        min(trait_df_logminmax$sensitivity_logminmax, na.rm = TRUE))
-  #   
-  #   # Binding normalization to the original trait dataset
-  #   trait_df <- cbind(trait_df, trait_df_logminmax)
-  #   
-  # } else if(norm == "rankminmax"){
-  #   # Rank normalization
-  #   trait_df_rankminmax <- apply(trait_df[, tracol], 2, function(x){
-  #     (dense_rank(x) - min(dense_rank(x), na.rm = TRUE))/(max(dense_rank(x), na.rm = TRUE) - min(dense_rank(x), na.rm = TRUE))
-  #   })
-  #   colnames(trait_df_rankminmax) <- paste0(colnames(trait_df_rankminmax), "_rankminmax")
-  #   
-  #   trait_df_rankminmax <- as.data.frame(trait_df_rankminmax)
-  #   
-  #   # Calculating the Sensitivity Index
-  #   trait_df_rankminmax$sensitivity_rankminmax <-
-  #     apply(trait_df_rankminmax, 1, function(x) sum(x, na.rm = TRUE))
-  #   
-  #   # Normalizing the Sensitivity Index
-  #   trait_df_rankminmax$sensitivity_rankminmax_norm <-
-  #     (trait_df_rankminmax$sensitivity_rankminmax -
-  #        min(trait_df_rankminmax$sensitivity_rankminmax, na.rm = TRUE))/
-  #     (max(trait_df_rankminmax$sensitivity_rankminmax, na.rm = TRUE) -
-  #        min(trait_df_rankminmax$sensitivity_rankminmax, na.rm = TRUE))
-  #   
-  #   # Binding normalization to the original trait dataset
-  #   trait_df <- cbind(trait_df, trait_df_rankminmax)
-  # }
-  # 
-  # 
-  # return(trait_df)
 }
 
 # 3. Running the function ----
@@ -174,4 +99,15 @@ test2 <- sensitivityFUN(trait_df = tr_birds,
 test3 <- sensitivityFUN(trait_df = tr_birds,
                         trait_columns = tracol,
                         norm_method = "rankminmax")
+# Plots
+library("ggplot2")
+sensitivity_Medians <- test %>% 
+  summarise(Median = median(sensitivity_minmax), na.rm = TRUE)
 
+ggplot(test, aes(sensitivity_minmax)) +
+  geom_histogram(color = "grey30", fill = "grey70") +
+  geom_segment(data = sensitivity_Medians, 
+               aes(x = Median, xend = Median, y = 0, yend = 22),
+               col = "dodgerblue", linewidth = 1) +
+  labs(x = "Normalized sensitivity", y = "Count") +
+  theme_bw()

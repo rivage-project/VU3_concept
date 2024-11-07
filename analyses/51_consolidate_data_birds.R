28051cd93d8d87807f4a0275b0a10f5019414f74
Gabriel Caetano <gabrielhoc@gmail.com>
2024-11-07 12:13:49 +0100

    Corrected consolidate data

--- analyses/51_consolidate_data_birds.R
+++ analyses/51_consolidate_data_birds.R
@@ -5,19 +5,21 @@ ias <- readRDS("data/derived-data/22_IAS_exposure_39_isl.rds")
 cc <- readRDS("data/derived-data/21_CC_SED_exposure_55_isl.rds")
 lu <- readRDS("data/derived-data/20_LU_exposure_55_isl.rds")
 
+ac_elev <- readRDS("data/derived-data/30_extrinsic_AC_elevation.rds")
+ac_PA <- readRDS("data/derived-data/30_extrinsic_AC_prop_PA.rds")
+ac_intr <- readRDS("data/derived-data/31_AC_intrinsic_BMP.rds")
+
 threats <- left_join(
-  lu %>% select(ID, mean_HM_static_2017, mean_HM_change, rdens_osm) %>%
-    mutate(ID = as.integer(ID)),
+  lu %>% select(ULM_ID, mean_HM_static_2017, mean_HM_change, rdens_osm) %>%
+    mutate(ULM_ID = as.integer(ULM_ID)),
   left_join(
-    cc %>% select(ID, sed_tot_med), 
+    cc %>% select(ULM_ID, sed_tot_med), 
     ias %>% select(ULM_ID,
                    nb_alien_bird, 
                    prop_alien_bird, 
-                   alien_bird_cover) %>%
-      rename(ID=ULM_ID)
+                   alien_bird_cover)
   )
-) %>%
-  rename(ULM_ID=ID)
+)
 
 birds <- readRDS("data/derived-data/12_bird_ckl_islands_clean.RDS")
 
@@ -29,6 +31,10 @@ data_birds_all <-
                    tr_birds,
                    by = "sci_name_IUCN") %>% 
   dplyr::left_join(threats,
+                   by = "ULM_ID") %>% 
+  dplyr::left_join(ac_elev,
+                   by = "ULM_ID") %>% 
+  dplyr::left_join(ac_PA[-5],
                    by = "ULM_ID")
 
 saveRDS(data_birds_all, "data_birds_all.rds")

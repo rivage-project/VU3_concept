d53cce8ed1aa7a3a27df0dff3a3e842849d3d1ca
AdrienneEtard <ucbteta@ucl.ac.uk>
2024-11-07 12:19:05 +0100

    Consolidate script - fixing ULM ID

--- analyses/51_consolidate_data_birds.R
+++ analyses/51_consolidate_data_birds.R
@@ -4,6 +4,9 @@ library(tidyverse)
 ias <- readRDS("data/derived-data/22_IAS_exposure_39_isl.rds")
 cc <- readRDS("data/derived-data/21_CC_SED_exposure_55_isl.rds")
 lu <- readRDS("data/derived-data/20_LU_exposure_55_isl.rds")
+colnames(lu)[colnames(lu)=='ID'] <- 'ULM_ID'
+colnames(ias)[colnames(ias)=='ID'] <- 'ULM_ID'
+colnames(cc)[colnames(cc)=='ID'] <- 'ULM_ID'
 
 ac_elev <- readRDS("data/derived-data/30_extrinsic_AC_elevation.rds")
 ac_PA <- readRDS("data/derived-data/30_extrinsic_AC_prop_PA.rds")
@@ -11,7 +14,7 @@ ac_intr <- readRDS("data/derived-data/31_AC_intrinsic_BMP.rds")
 
 threats <- left_join(
   lu %>% select(ULM_ID, mean_HM_static_2017, mean_HM_change, rdens_osm) %>%
-    mutate(ULM_ID = as.integer(ULM_ID)),
+  mutate(ULM_ID = as.integer(ULM_ID)),
   left_join(
     cc %>% select(ULM_ID, sed_tot_med), 
     ias %>% select(ULM_ID,
